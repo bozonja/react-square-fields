ca513678c03ee0aa71397072022ffcd1c7c3c5a1
Josip Markovic <bozadesign@gmail.com>
2020-09-23 16:05:14 +0200

    Updates

---
+++ precache-manifest.fed60f520392c68b6093db144778d5b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cef15c7115829d77dd6fc726902822",
+    "url": "/react-square-fields/index.html"
+  },
+  {
+    "revision": "17a9f3f6591a129f595f",
+    "url": "/react-square-fields/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4bf423cace33314201c",
+    "url": "/react-square-fields/static/js/2.38985cc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-square-fields/static/js/2.38985cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a9f3f6591a129f595f",
+    "url": "/react-square-fields/static/js/main.576bf98b.chunk.js"
+  },
+  {
+    "revision": "aa6df059e0abf5872474",
+    "url": "/react-square-fields/static/js/runtime-main.b71a7570.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-square-fields/static/media/logo.5d5d9eef.svg"
+  }
+]);
